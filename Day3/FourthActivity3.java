24789500d8346ebc15fd5228903d057719a402ab
mo-tanveer <mdtanveer8299@gmail.com>
2026-01-06 23:14:58 +0530

    code of day 3

---
+++ Day3/FourthActivity3.java
@@ -0,0 +1,14 @@
+public class FourthActivity3 
+  {
+    public static void main(String args[])
+     {
+       int a = 10, b = 20, c = 30;
+
+
+
+       System.out.println((c>b) || (b>a));
+       System.out.println((c<b) || (b>a));
+       System.out.println((c>b) || (b<a));
+       System.out.println((c<b) || (b<a));
+   }
+}
