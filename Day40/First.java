217feb09643a5712e4f9c1bf5db5a0da1432c037
mo-tanveer <mdtanveer8299@gmail.com>
2026-05-07 17:33:48 +0530

    End of core

---
+++ Day40/First.java
@@ -0,0 +1,15 @@
+package com.app;
+import java.util.*;
+
+public class First{
+public static void main(String args[]){
+
+    Map<String , String> map = new HashMap<String ,String>();
+
+	map.put("Name","Tanveer");
+	map.put("Phone","8229555112");
+	map.put("Address","BBk");
+
+System.out.println("Elements in map::" + map);
+}
+}
